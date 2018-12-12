b5e130e8d2100d2ac85c1c74b1fff9c370808583
zero2603 <20153713@student.hust.edu.vn>
2018-12-12 21:58:38 +0700

    update comment model

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['user_id', 'product_id', 'parent_comment_id', 'content', 'rating'];
+    public $timestamps = true;
 }

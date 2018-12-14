293a99792d56c19060e8fa60b4b081d56d780564
hieu <havenoescape@gmail.com>
2018-12-15 00:03:35 +0700

    cut frontend

--- resources/views/user/products/dress.blade.php
+++ resources/views/user/products/dress.blade.php
@@ -52,24 +52,7 @@
                         </div>
                     </div>
 
-                    <!-- ##### Single Widget ##### -->
-                    <div class="widget price mb-50">
-                        <!-- Widget Title -->
-                        <h6 class="widget-title mb-30">Filter by</h6>
-                        <!-- Widget Title 2 -->
-                        <p class="widget-title2 mb-30">Price</p>
-
-                        <div class="widget-desc">
-                            <div class="slider-range">
-                                <div data-min="49" data-max="360" data-unit="$" class="slider-range-price ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" data-value-min="49" data-value-max="360" data-label-result="Range:">
-                                    <div class="ui-slider-range ui-widget-header ui-corner-all"></div>
-                                    <span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0"></span>
-                                    <span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0"></span>
-                                </div>
-                                <div class="range-price">Range: $49.00 - $360.00</div>
-                            </div>
-                        </div>
-                    </div>
+                   
 
                     <!-- ##### Single Widget ##### -->
                     <div class="widget color mb-50">
@@ -77,34 +60,20 @@
                         <p class="widget-title2 mb-30">Color</p>
                         <div class="widget-desc">
                             <ul class="d-flex">
-                                <li><a href="#" class="color1"></a></li>
-                                <li><a href="#" class="color2"></a></li>
-                                <li><a href="#" class="color3"></a></li>
-                                <li><a href="#" class="color4"></a></li>
-                                <li><a href="#" class="color5"></a></li>
-                                <li><a href="#" class="color6"></a></li>
-                                <li><a href="#" class="color7"></a></li>
-                                <li><a href="#" class="color8"></a></li>
-                                <li><a href="#" class="color9"></a></li>
-                                <li><a href="#" class="color10"></a></li>
+                                <li><a href="/products?color=Blue" class="color4" id="Blue" ></a></li>
+                                <li><a href="/products?color=Gray" class="color2" id="Gray" ></a></li>
+                                <li><a href="/products?color=Black" class="color3" id="Black" ></a></li>
+                                <li><a href="/products?color=White" class="color1" id="White" ></a></li>
+                                <li><a href="/products?color=Pink" class="color5" id="Pink" ></a></li>
+                                <li><a href="/products?color=Yellow" class="color6" id="Yellow" ></a></li>
+                                <li><a href="/products?color=Orange" class="color7" id="Orange" ></a></li>
+                                <li><a href="/products?color=Brown" class="color8" id="Brown" ></a></li>
+                                <li><a href="/products?color=Green" class="color9" id="Green" ></a></li>
+                                <li><a href="/products?color=Purple" class="color10" id="Purple" ></a></li>
                             </ul>
                         </div>
                     </div>
 
-                    <!-- ##### Single Widget ##### -->
-                    <div class="widget brands mb-50">
-                        <!-- Widget Title 2 -->
-                        <p class="widget-title2 mb-30">Brands</p>
-                        <div class="widget-desc">
-                            <ul>
-                                <li><a href="#">Asos</a></li>
-                                <li><a href="#">Mango</a></li>
-                                <li><a href="#">River Island</a></li>
-                                <li><a href="#">Topshop</a></li>
-                                <li><a href="#">Zara</a></li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
             </div>
 
